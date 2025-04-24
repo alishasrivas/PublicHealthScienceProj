ae05dd79c54cbde19eadb7668f550b513b10e357
erinhlosee <elosee@umass.edu>
2025-04-24 12:25:49 -0400

    the script that actually works (no filtering)

--- finaldatasetscript.R
+++ finaldatasetscript.R
@@ -3,13 +3,16 @@ library(dplyr)
 
 inpatient_encounters <- read.csv("inpatient_encounters.csv")
 inpatient_encounters <- inpatient_encounters |>
-  transform(BENE_ID = as.character(BENE_ID))
+  mutate(BENE_ID = as.character(row_number()))
+glimpse(inpatient_encounters)
+
 enrollment <- read.csv("enrollment.csv")
+enrollment <- enrollment |>
+  mutate(BENE_ID = as.character(row_number()))
 glimpse(enrollment)
 
-enrollment <- enrollment |>
-  transform(BENE_ID = as.character(BENE_ID))
-fin_enrollment <- select(enrollment, BENE_ID, BENE_BIRTH_DT)
 
 
-fin_dataset <- full_join(inpatient_encounters, fin_enrollment)
+inpatient_encounters_final <- inpatient_encounters |>
+  left_join(fin_enrollment, by = "BENE_ID")
+
